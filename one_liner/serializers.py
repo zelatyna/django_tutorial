aa51018df7edfbf81d7a4d95c703eb83c87be9a4
zelatyna <magda.sendal@gmail.com>
2019-11-25 22:41:42 +0100

    build rest serializers

--- one_liner/serializers.py
+++ one_liner/serializers.py
@@ -9,9 +9,23 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class OneLinerSerializer(serializers.HyperlinkedModelSerializer):
-    #author = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name='author')
+
     author = UserSerializer(many=False, read_only=True)
     class Meta:
         model = One_liner
         fields = ['pub_date', 'one_liner_text', 'author']
 
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return One_liner.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.one_liner_text = validated_data.get('one_liner_text', instance.one_liner_text)
+        instance.pub_date = validated_data.get('pub_date', instance.pub_date)
+        instance.save()
+        return instance
--- one_liner/urls.py
+++ one_liner/urls.py
@@ -5,13 +5,15 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'one_liner_txt', views.OneLinerViewSet)
-router.register(r'one_liner_user', views.UserViewSet)
+#router.register(r'one_liner_txt', views.updates_list)
+router.register(r'users', views.UserViewSet)
+
 
 
 app_name= 'one_liner'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('updates/', views.UpdatesList.as_view())
 ]
