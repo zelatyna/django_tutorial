6278fe8a0cff031737c47d9b21f45c1b3ab2f134
zelatyna <magda.sendal@gmail.com>
2019-11-26 17:42:38 +0100

    make the post api call to work with FK to user

--- one_liner/serializers.py
+++ one_liner/serializers.py
@@ -3,22 +3,26 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    #updates = serializers.StringRelatedField(many=True)
     class Meta:
         model = User
-        fields = ['user_name']
+        fields = ['user_name', 'id']
 
 
 class OneLinerSerializer(serializers.HyperlinkedModelSerializer):
 
     author = UserSerializer(many=False, read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = One_liner
-        fields = ['pub_date', 'one_liner_text', 'author']
+        fields = ['pub_date', 'one_liner_text', 'author', 'author', 'user_id']
 
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
         """
+        validated_data['author'] = User.objects.filter(id = validated_data.pop('user_id')).first()
         return One_liner.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
@@ -27,5 +31,6 @@ class OneLinerSerializer(serializers.HyperlinkedModelSerializer):
         """
         instance.one_liner_text = validated_data.get('one_liner_text', instance.one_liner_text)
         instance.pub_date = validated_data.get('pub_date', instance.pub_date)
+        instance.author = validated_data.get('author', instance.author)
         instance.save()
         return instance
