f623cc28d658e72e13bfabc44266ffbc7dffa4be
zelatyna <magda.sendal@gmail.com>
2019-12-19 16:27:13 +0100

    implement customized authentication

--- one_liner/admin.py
+++ one_liner/admin.py
@@ -1,24 +1,106 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, One_liner
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username', 'phone_number']
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import CustomUser
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'phone_number')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password', 'phone_number', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('username', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('phone_number',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'username', 'phone_number'),
-        }),
+            'fields': ('username', 'phone_number', 'password1', 'password2')}
+        ),
     )
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+# Now register the new UserAdmin...
+admin.site.register(CustomUser, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
+
+
+
+
+# class CustomUserAdmin(UserAdmin):
+#     #add_form = CustomUserCreationForm
+#     #form = CustomUserChangeForm
+#     model = CustomUser
+#     list_display = ['username', 'phone_number']
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': ('username', 'password', 'phone_number'),
+    #     }),
+    # )
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -27,7 +109,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(One_liner, QuestionAdmin)
 
 # admin.site.register(User, UserAdmin)
