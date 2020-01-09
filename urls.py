4a3dd372ea8e5494c7b35a25c86454c8cd502635
zelatyna <magda.sendal@gmail.com>
2020-01-09 15:18:42 +0100

    remove unused apps

--- urls.py
+++ urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('one_liner/', include('one_liner.urls')),
-
     path('admin/', admin.site.urls),
 ]
