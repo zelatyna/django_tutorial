9cf46084fb690d8ad94333e03d2a369ceaa394ea
zelatyna <magda.sendal@gmail.com>
2020-01-16 16:33:49 +0100

    remove duplicate files

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('one_liner/', include('one_liner.urls')),
-    path('admin/', admin.site.urls),
-]
