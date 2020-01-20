9a9253354d7bf8af211f7a6626a8f10989dbaf70
zelatyna <magda.sendal@gmail.com>
2020-01-20 13:06:20 +0100

    add work in progress

--- one_liner/views.py
+++ one_liner/views.py
@@ -29,23 +29,29 @@ class IndexView(generic.ListView):
     template_name = 'one_liner/update_view.html'
     context_object_name = 'latest_question_list'
 
-    def get_queryset(self, column_count=4, max_items=20):
+    def get_queryset(self, column_count=4, max_items=40):
         """Return the last five published questions."""
 
         qs = One_liner.objects.order_by('-pub_date')[:max_items]
+        return qs
+        # reorder the result items to be displayed from left to right in masonry display
+        # rows_count = int(len(qs)/column_count)
+        # if len(qs) % column_count >0:
+        #     rows_count+=1
+        # index_matrix = np.concatenate((np.arange(len(qs)), (-1)*np.ones(rows_count*column_count - len(qs)))).reshape(rows_count, column_count)
+        # index_matrix = np.matrix(index_matrix)
+        # print(index_matrix)
+        # index_list = index_matrix.transpose().reshape(1,rows_count*column_count).tolist()[0]
+        # print(index_list)
+        # index_list = [x for x in index_list if int(x) != -1]
+        # print(index_list)
+        # reordered =  [None] * len(qs)
+        # for id, item in enumerate(qs):
+        #     print(id, qs[id], int(index_list[id]) ,  qs[int(index_list[id])])
+        #     reordered[id] = qs[int(index_list[id])]
 
-        #reorder the result items to be displayed from left to right in masonry display
-        rows_count = int(max_items/column_count)
-        index_matrix = np.concatenate((np.arange(len(qs)), (-1)*np.ones(max_items - len(qs)))).reshape(rows_count, column_count)
-        index_matrix = np.matrix(index_matrix)
-        index_list = index_matrix.transpose().reshape(1,rows_count*column_count).tolist()[0]
-        index_list = [x for x in index_list if int(x) != -1]
-        reordered =  [None] * len(qs)
-        for id, item in enumerate(qs):
-            reordered[index_list.index(id)] = item
 
-
-        return reordered
+        # return reordered
 
 
 class UpdatesList(mixins.ListModelMixin,
